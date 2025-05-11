97881e7b15bd364ea9f0bb1ecefa8a91108273b1
Ilya <thesudro@gmail.com>
2025-05-11 16:27:52 +0300

    Change next config

--- next.config.js
+++ next.config.js
@@ -1,14 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'fakestoreapi.com',
-        port: '',
-        pathname: '/img/**',
-      },
-    ],
+    domains: ['raw.githubusercontent.com', 'pokeapi.co'],
+    formats: ['image/avif', 'image/webp'],
+  },
+
+  typescript: {
+    ignoreBuildErrors: false,
+  },
+
+  eslint: {
+    ignoreDuringBuilds: false,
   },
 }
 
